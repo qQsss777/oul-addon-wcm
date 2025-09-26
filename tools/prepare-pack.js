35f0be0f0545046a2cefa9376423bf64d6ff07c1
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-09-26 10:28:14 +0200

    fix: update prepare tools for ubuntu

--- tools/prepare-pack.js
+++ tools/prepare-pack.js
@@ -1,12 +1,9 @@
 const fs = require("fs");
-const path = require("path");
 const args = process.argv.slice(2);
 const [target, folder, name] = args;
 if (fs.existsSync(folder)) {
   fs.rmSync(folder, { recursive: true, force: true });
 }
 fs.mkdirSync(folder);
-const pathToNode = path.join(__dirname, target);
-const pathToCopy = folder + "\\" + name;
-console.log(pathToCopy);
-fs.copyFile(pathToNode, pathToCopy, () => {});
+const pathToCopy = folder + "/" + name;
+fs.copyFile(target, pathToCopy, () => {});
